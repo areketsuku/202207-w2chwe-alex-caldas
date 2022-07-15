0c02d8193b5a6f9358b34c170ce8648c45a353c5
Alex Caldas <alexcaldasaixarch@gmail.com>
2022-07-15 17:54:38 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+    "import/extensions": ["error", "always"],
+  },
+};
