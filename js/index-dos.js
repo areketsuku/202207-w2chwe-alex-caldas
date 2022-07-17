73c162f8c3f0acb1569fc71d90216aa60c515f60
Alex Caldas <alexcaldasaixarch@gmail.com>
2022-07-17 15:40:25 +0200

    feature: added header with control option over grid

--- js/index-dos.js
+++ js/index-dos.js
@@ -1,8 +1,17 @@
-let interval;
-let paint = true;
+import Grid from "./Class/Grid.js";
+
+let ThisGrid = new Grid(5, 5);
+let gridWidth;
+let gridHeight;
+
+document.getElementById("submit").addEventListener("click", () => {
+  gridWidth = document.getElementById("width").value;
+  gridHeight = document.getElementById("height").value;
+  ThisGrid = new Grid(gridHeight, gridWidth);
+});
 
 document.addEventListener("click", (e) => {
-  if (paint) {
+  if (ThisGrid.paint) {
     if (e.target.className === "alive") {
       e.target.classList.remove("alive");
     } else {
@@ -11,15 +20,6 @@ document.addEventListener("click", (e) => {
   }
 });
 
-document.getElementById("start-stop").addEventListener("change", (e) => {
-  if (document.getElementById("start-stop").checked) {
-    paint = false;
-    interval = window.setInterval(() => {
-      console.log("a");
-    }, 1000);
-  } else {
-    window.clearInterval(interval);
-    interval = null;
-    paint = true;
-  }
+document.getElementById("start-stop").addEventListener("change", () => {
+  ThisGrid.startGame();
 });
