5acafd40bc131529b9b5cc9263830cc51e5b71ef
Alex Caldas <alexcaldasaixarch@gmail.com>
2022-07-17 11:03:24 +0200

    bugfix: refactored function to reduce complexity on line 33

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const div = document.getElementsByTagName("div");
 const gridWidth = 5;
 const gridHeight = 5;
 
-const setNeighboursCountersToZero = () => {
+const setNeighborsCountersToZero = () => {
   let y = 0;
   for (y; y < gridHeight; y += 1) {
     let x = 0;
     for (x; x < gridWidth; x += 1) {
-      div[y].children[x].setAttribute("data-neighbours", 0);
+      div[y].children[x].setAttribute("data-neighbors", 0);
     }
   }
 };
@@ -18,34 +18,33 @@ const reviveCells = () => {
     let x = 0;
     for (x; x < gridWidth; x += 1) {
       const cell = div[y].children[x];
-      const cellNeighbours = cell.getAttribute("data-neighbours");
+      const cellNeighbors = cell.getAttribute("data-neighbors");
       if (cell.classList[0] !== "alive") {
-        if (cellNeighbours > 2) {
+        if (cellNeighbors > 2) {
           cell.classList.add("alive");
         }
-      } else if (cellNeighbours < 2 || cellNeighbours > 3) {
+      } else if (cellNeighbors < 2 || cellNeighbors > 3) {
         cell.classList.remove("alive");
       }
     }
   }
 };
+const introduceCellToNeighbors = (y, x) => {
+  const neighbor = div[y].children[x];
+  let neighborCounter = neighbor.getAttribute("data-neighbors");
+  neighbor.setAttribute("data-neighbors", Number(neighborCounter) + 1);
+  neighborCounter = neighbor.getAttribute("data-neighbors");
+};
 
-const checkForNeighbours = (y, x) => {
+const checkForNeighbors = (y, x) => {
   let my = y - 1;
   for (my; my <= y + 1; my += 1) {
     let mx = x - 1;
     for (mx; mx <= x + 1; mx += 1) {
       if (div[my] !== undefined) {
         if (div[my].children[mx] !== undefined) {
-          if (mx === x && my === y) {
-          } else {
-            const neighbour = div[my].children[mx];
-            let neighbourCounter = neighbour.getAttribute("data-neighbours");
-            neighbour.setAttribute(
-              "data-neighbours",
-              Number(neighbourCounter) + 1
-            );
-            neighbourCounter = neighbour.getAttribute("data-neighbours");
+          if (mx !== x || my !== y) {
+            introduceCellToNeighbors(my, mx);
           }
         }
       }
@@ -59,17 +58,14 @@ const checkForAlive = () => {
     let x = 0;
     for (x; x < gridWidth; x += 1) {
       if (div[y].children[x].classList[0] === "alive") {
-        checkForNeighbours(y, x);
+        checkForNeighbors(y, x);
       }
     }
   }
   reviveCells();
-  setNeighboursCountersToZero();
+  setNeighborsCountersToZero();
 };
 
 setInterval(() => {
   checkForAlive();
 }, 100);
-
-// 0 --> min 3 alive neighbours --> 1
-// 1 --> inf a 2 alive neighbours or sup a 3 alive neighbours --> 0
